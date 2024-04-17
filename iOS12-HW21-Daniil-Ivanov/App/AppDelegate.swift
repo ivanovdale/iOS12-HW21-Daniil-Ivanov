2e5151a739bd6a8ac0e10451275db20a0869af3c
ivanovdale <dania-spb@mail.ru>
2024-04-17 16:29:35 +0300

    Initial commit

---
+++ iOS12-HW21-Daniil-Ivanov/App/AppDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  AppDelegate.swift
+//  iOS12-HW21-Daniil-Ivanov
+//
+//  Created by Daniil (work) on 17.04.2024.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate { }
+
---
+++ iOS12-HW21-Daniil-Ivanov/App/SceneDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  SceneDelegate.swift
+//  iOS12-HW21-Daniil-Ivanov
+//
+//  Created by Daniil (work) on 17.04.2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+}
+
